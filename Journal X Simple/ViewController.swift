3aa6c38fc4632fc751da5df32ea2a574824ec758
Amogh Joshi <amoghjoshi1999@gmail.com>
2020-02-01 18:22:33 -0500

    Initial Commit

---
+++ Journal X Simple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal X Simple
+//
+//  Created by Amogh Joshi on 2/1/20.
+//  Copyright © 2020 Amogh Joshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
